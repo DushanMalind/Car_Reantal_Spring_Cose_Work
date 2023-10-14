b0bd3e2671f69a78c0b6a047028e6a3a33cb481c
DushanMalind <dushanmalinda010@gmail.com>
2023-10-14 22:50:21 +0530

    Spring Car Rental

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/WebAppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.carRental;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 22:15
+ * 14/10/2023
+ * ASUS
+ * Car_Rental_Project
+ * lk.ijse.spring.carRental
+ */
+public class WebAppInitializer {
+}
