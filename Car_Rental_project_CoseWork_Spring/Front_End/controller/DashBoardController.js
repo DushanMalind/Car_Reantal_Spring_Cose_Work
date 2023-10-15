388df80b69e1fa8aeeeb9ca3bbbc38f29674028d
DushanMalind <dushanmalinda010@gmail.com>
2023-10-15 23:57:34 +0530

    login button open close

---
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/DashBoardController.js
@@ -0,0 +1,102 @@
+/*
+// This is the controller for the dashboard page
+
+
+$("#btnLogin").click(function () {
+  $("#login_page_container").css('transform', 'scale(1)');
+  $("#ForgotPasswordPage").css('transform', 'scale(0)');
+});
+
+
+$("#btnCancelLogin").click(function () {
+  $("#login_page_container").css('transform', 'scale(0)');
+  $("#ForgotPasswordPage").css('transform', 'scale(0)');
+});
+
+
+$("#btnRegisterInLoginPage").click(function () {
+  $("#login_page_container").css('transform', 'scale(0)');
+  $("#ForgotPasswordPage").css('transform', 'scale(0)');
+
+});
+
+
+$("#btnForgotPW").click(function () {
+  $("#login_page_container").css('transform', 'scale(0)');
+  $("#ForgotPasswordPage").css('transform', 'scale(1)');
+
+});
+
+
+$("#btnCancelForgotPW").click(function () {
+  $("#login_page_container").css('transform', 'scale(0)');
+  $("#ForgotPasswordPage").css('transform', 'scale(0)');
+});
+*/
+
+
+
+initiateUI();
+
+function initiateUI(){
+  clearAll();
+  $("#login_page_container").css("display","block");
+  $("#ForgotPasswordPage").css("display","block");
+
+  setTheLastView();
+}
+
+function saveLastView(clickedID){
+  switch (clickedID){
+    case "login_page_container":
+      localStorage.setItem("view","LOGIN");
+      break;
+    case "ForgotPasswordPage":
+      localStorage.setItem("view","FORGOTPASSWORD");
+      break;
+  }
+}
+
+function setTheLastView(){
+  let view=localStorage.getItem("view");
+  switch (view){
+    case "LOGIN":
+      setView($("#login_page_container"));
+      break;
+    case "FORGOTPASSWORD":
+      setView($("#ForgotPasswordPage"));
+      break;
+    /*default:
+      setView($("#login_page_container"));*/
+  }
+}
+
+function clearAll(){
+  $("#login_page_container").css("display","none");
+  $("#ForgotPasswordPage").css("display","none");
+}
+
+function setView(viewOb){
+  clearAll();
+  viewOb.css("display","block");
+  saveLastView(viewOb.get(0).id);
+  console.log(viewOb.get(0).id);
+}
+
+$("#btnLogin").click(function () {
+  setView($("#login_page_container"));
+
+});
+
+$("#btnCancelLogin").click(function () {
+  $("#login_page_container").css("display","none");
+
+});
+
+$("#btnForgotPW").click(function () {
+  setView($("#ForgotPasswordPage"));
+});
+
+$("#btnCancelForgotPW").click(function () {
+  setView($("#login_page_container"));
+});
