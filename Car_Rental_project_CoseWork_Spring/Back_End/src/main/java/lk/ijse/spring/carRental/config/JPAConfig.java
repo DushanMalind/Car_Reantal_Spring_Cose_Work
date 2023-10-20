0f756d850b904b00c713dad4a8a32382a7d49904
DushanMalind <dushanmalinda010@gmail.com>
2023-10-20 11:26:30 +0530

    LocalContainerEntityManagerFactoryBean create

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/JPAConfig.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/JPAConfig.java
@@ -2,10 +2,15 @@ package lk.ijse.spring.carRental.config;
 
 import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * `@authority` DUSHAN MALINDA
@@ -24,6 +29,17 @@ public class JPAConfig {
 
     @Autowired
     Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource ds, JpaVendorAdapter vad){
+        LocalContainerEntityManagerFactoryBean factory= new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(ds);
+        factory.setJpaVendorAdapter(vad);
+        //factory.setPackagesToScan(env.getRequiredProperty("pro.entity"));
+        factory.setPackagesToScan("lk.ijse.spring.carRental.entity");
+        return factory;
+
+    }
 
 
 }
