344f9c9603e1b0785e71ddd94e8460630f47cf42
DushanMalind <dushanmalinda010@gmail.com>
2023-10-20 14:56:16 +0530

    DriverDTO  Create

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/dto/DriverDTO.java
@@ -0,0 +1,31 @@
+package lk.ijse.spring.carRental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 14:55
+ * 20/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.dto
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class DriverDTO {
+    private String driverId;
+    private String driverEmail;
+    private String driverFirstName;
+    private String driverLastName;
+    private String driverNICNumber;
+    private String driverDrivingLicenseId;
+    private String driverContactNumber;
+    private String driverAddress;
+    private String availability;
+}
