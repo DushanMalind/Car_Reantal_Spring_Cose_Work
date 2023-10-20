6a2cf21e1d135721df21e90459e7cd564f3de425
DushanMalind <dushanmalinda010@gmail.com>
2023-10-20 10:40:28 +0530

    config class Create

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/WebAppConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.carRental.config;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 10:39
+ * 20/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.config
+ */
+public class WebAppConfig {
+}
---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/WebRootConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.carRental.config;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 10:40
+ * 20/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.config
+ */
+public class WebRootConfig {
+}
