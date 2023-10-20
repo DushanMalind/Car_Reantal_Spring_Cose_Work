6e06b85e7a06a30039db33e6d3f9514e9bcd710e
DushanMalind <dushanmalinda010@gmail.com>
2023-10-20 12:21:38 +0530

    AppWideExceptionHandler Create class and Expection

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/advisor/AppWideExceptionHandler.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/advisor/AppWideExceptionHandler.java
@@ -1,10 +1,13 @@
 package lk.ijse.spring.carRental.advisor;
 
+import lk.ijse.spring.carRental.util.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
 
 /**
  * `@authority` DUSHAN MALINDA
@@ -20,5 +23,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AppWideExceptionHandler {
     @ExceptionHandler({RuntimeException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseUtil handleException(RuntimeException e){
+        return new ResponseUtil("Error",e.getMessage(),"");
+    }
 
+    @ExceptionHandler({ClassNotFoundException.class})
+    public ResponseUtil handleAllClassNotFoundExceptions(ClassNotFoundException e){
+        return new ResponseUtil("Error",e.getMessage(),null);
+    }
+
+    @ExceptionHandler({SQLException.class})
+    public ResponseUtil handleAllSQLExceptions(SQLException e){
+        return new ResponseUtil("Error",e.getMessage(),null);
+    }
 }
