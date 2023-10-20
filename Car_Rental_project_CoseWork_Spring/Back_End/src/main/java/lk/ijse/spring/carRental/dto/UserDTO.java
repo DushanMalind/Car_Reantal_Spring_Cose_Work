b2e9ca5e0bc0a2360f1b44aad500262de01c8368
DushanMalind <dushanmalinda010@gmail.com>
2023-10-20 12:59:46 +0530

    UserDTO Create

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package lk.ijse.spring.carRental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 12:58
+ * 20/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.dto
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class UserDTO {
+    private String userId;
+    private String username;
+    private String password;
+}
