b133572e9f0f97f0d7ea370d54555cb7b90267f8
DushanMalind <dushanmalinda010@gmail.com>
2023-10-20 11:50:26 +0530

    WebAppInitializer extends

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/WebAppInitializer.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/WebAppInitializer.java
@@ -1,5 +1,9 @@
 package lk.ijse.spring.carRental;
 
+import lk.ijse.spring.carRental.config.WebAppConfig;
+import lk.ijse.spring.carRental.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * `@authority` DUSHAN MALINDA
  * 22:15
@@ -8,5 +12,19 @@ package lk.ijse.spring.carRental;
  * Car_Rental_Project
  * lk.ijse.spring.carRental
  */
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
