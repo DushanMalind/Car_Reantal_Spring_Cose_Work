1fab1248aae79aecfd4cdaf7638e9866ab05a7af
DushanMalind <dushanmalinda010@gmail.com>
2023-10-20 11:34:44 +0530

    DataSource create

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/JPAConfig.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/JPAConfig.java
@@ -1,11 +1,13 @@
 package lk.ijse.spring.carRental.config;
 
-import org.hibernate.cfg.Environment;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -35,11 +37,21 @@ public class JPAConfig {
         LocalContainerEntityManagerFactoryBean factory= new LocalContainerEntityManagerFactoryBean();
         factory.setDataSource(ds);
         factory.setJpaVendorAdapter(vad);
-        //factory.setPackagesToScan(env.getRequiredProperty("pro.entity"));
-        factory.setPackagesToScan("lk.ijse.spring.carRental.entity");
+        factory.setPackagesToScan(env.getRequiredProperty("pro.entity"));
         return factory;
 
     }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds= new DriverManagerDataSource();
+        ds.setUsername(env.getRequiredProperty("pro.username"));
+        ds.setPassword(env.getRequiredProperty("pro.password"));
+        ds.setDriverClassName(env.getRequiredProperty("pro.driver"));
+        ds.setUrl(env.getRequiredProperty("pro.url"));
+        return ds;
+    }
+
 
 
 }
