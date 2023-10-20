e8a3d3cf2688758c9e2a0af6af99a2ca4498be15
DushanMalind <dushanmalinda010@gmail.com>
2023-10-20 10:56:02 +0530

    webAppConfig and webRootConfig class Create

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/WebAppConfig.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/WebAppConfig.java
@@ -1,5 +1,9 @@
 package lk.ijse.spring.carRental.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * `@authority` DUSHAN MALINDA
  * 10:39
@@ -8,5 +12,12 @@ package lk.ijse.spring.carRental.config;
  * Back_End
  * lk.ijse.spring.carRental.config
  */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.ijse.spring.carRental.controller")
 public class WebAppConfig {
+    public WebAppConfig() {
+        System.out.println("WebAppConfig:Web App Instantiated");
+    }
 }
--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/WebRootConfig.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/config/WebRootConfig.java
@@ -1,5 +1,11 @@
 package lk.ijse.spring.carRental.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 /**
  * `@authority` DUSHAN MALINDA
  * 10:40
@@ -8,5 +14,18 @@ package lk.ijse.spring.carRental.config;
  * Back_End
  * lk.ijse.spring.carRental.config
  */
+
+
+@Configuration
+@Import({JPAConfig.class})
+@ComponentScan(basePackages = "lk.ijse.spring.carRental.service")
 public class WebRootConfig {
+    public WebRootConfig() {
+        System.out.println("WebRootConfig:Web Root Instantiated");
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
