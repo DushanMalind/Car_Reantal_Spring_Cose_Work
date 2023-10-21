79536c113aec006f65daf0e17d39991f33fe57cb
DushanMalind <dushanmalinda010@gmail.com>
2023-10-21 10:53:26 +0530

    Customer Entity Create

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/entity/Customer.java
@@ -0,0 +1,40 @@
+package lk.ijse.spring.carRental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 10:43
+ * 21/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.entity
+ */
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Customer {
+    @Id
+    private String customerId;
+    private String customerName;
+    private String customerEmail;
+    private String customerNIC;
+    private String customerAddress;
+    private String customerContact;
+    private String customerDrivingLicenseId;
+    private String customerDrivingLicenseImage;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Users users;
+}
