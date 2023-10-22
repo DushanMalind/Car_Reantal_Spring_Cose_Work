221dc96585e2d9f6cc70aca0d96484707132b735
DushanMalind <dushanmalinda010@gmail.com>
2023-10-22 18:24:54 +0530

    CustomerController Class Create

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/controller/CustomerController.java
@@ -0,0 +1,19 @@
+package lk.ijse.spring.carRental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 18:18
+ * 22/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.controller
+ */
+@RestController
+@RequestMapping("/customer")
+@CrossOrigin
+public class CustomerController {
+}
