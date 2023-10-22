72b5da1ccc347be57b2ef7953c15711cd83072d8
DushanMalind <dushanmalinda010@gmail.com>
2023-10-22 13:32:23 +0530

    UserController method Create

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/controller/UserController.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.carRental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 13:29
+ * 22/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.controller
+ */
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController {
+
+}
