8454387c5c5b9650095c2696531b97390a03136c
DushanMalind <dushanmalinda010@gmail.com>
2023-10-24 12:40:50 +0530

    LoginFrom userBane

--- Car_Rental_project_CoseWork_Spring/Front_End/controller/LoginFrom.js
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/LoginFrom.js
@@ -1,3 +1,17 @@
 
 var regExLoginUserName= /^[A-Z|a-z\s]{3,20}$/;
 var regExLoginPassword = /^[A-Z|a-z\s|@|#|$|0-9]{6,10}$/;
+
+
+$("#loginUsername").keyup(function (event) {
+  let username = $("#loginUsername").val();
+  if (regExLoginUserName.test(username)) {
+    $("#loginUsername").css('border', '2px solid #31d2f2');
+    if (event.key == "Enter") {
+      $("#loginPassword").focus();
+    }
+    else {
+      $("#loginUsername").css('border', '2px solid red');
+    }
+  }
+});
