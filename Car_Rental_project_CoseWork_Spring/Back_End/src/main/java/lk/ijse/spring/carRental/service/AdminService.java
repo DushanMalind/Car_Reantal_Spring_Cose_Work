a8eec5fa2bcf484469065e55bdc0db0fbf8e2039
DushanMalind <dushanmalinda010@gmail.com>
2023-10-25 13:54:38 +0530

    AdminService  interface Create

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/service/AdminService.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.carRental.service;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 13:53
+ * 25/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.service
+ */
+public class AdminService {
+
+}
