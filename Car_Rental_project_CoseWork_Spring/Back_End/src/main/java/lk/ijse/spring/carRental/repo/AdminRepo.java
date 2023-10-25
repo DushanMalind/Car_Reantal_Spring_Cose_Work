96473a6935d4f15a1d867e447a9b3d2701c02e21
DushanMalind <dushanmalinda010@gmail.com>
2023-10-25 14:01:12 +0530

    AdminRepo interface findByPasswordAndName Create

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/repo/AdminRepo.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/repo/AdminRepo.java
@@ -13,4 +13,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface AdminRepo extends JpaRepository<Admin,String> {
 
+    Admin findByPasswordAndName(String password,String name);
 }
