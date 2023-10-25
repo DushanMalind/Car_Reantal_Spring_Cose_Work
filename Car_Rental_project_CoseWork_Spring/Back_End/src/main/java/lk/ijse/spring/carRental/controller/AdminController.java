c0947cb83f5854250cfb400e893af495fe1c16ab
DushanMalind <dushanmalinda010@gmail.com>
2023-10-25 14:30:35 +0530

    AdminController Class

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/controller/AdminController.java
@@ -0,0 +1,21 @@
+package lk.ijse.spring.carRental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 14:27
+ * 25/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.controller
+ */
+
+@RestController
+@RequestMapping("/admin")
+@CrossOrigin
+public class AdminController {
+
+}
