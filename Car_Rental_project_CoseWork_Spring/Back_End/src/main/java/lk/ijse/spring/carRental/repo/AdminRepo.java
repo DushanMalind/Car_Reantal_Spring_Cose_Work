abc85d55f6fb0b957a8488dbfb5c8e2a5371df73
DushanMalind <dushanmalinda010@gmail.com>
2023-10-25 13:53:00 +0530

    Admin Repo interface Craete

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/repo/AdminRepo.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.carRental.repo;
+
+import lk.ijse.spring.carRental.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 13:51
+ * 25/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.repo
+ */
+public interface AdminRepo extends JpaRepository<Admin,String> {
+}
