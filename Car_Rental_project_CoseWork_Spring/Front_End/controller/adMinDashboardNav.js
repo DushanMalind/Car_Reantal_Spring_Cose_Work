25b9d67eab1cef1caf92d766b05549e62417fd3e
DushanMalind <dushanmalinda010@gmail.com>
2023-10-26 15:32:54 +0530

    adMinDashboard Js Part

---
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/adMinDashboardNav.js
@@ -0,0 +1,24 @@
+
+
+$('#admin_dashboard').css('display', 'block');
+$("#car_dashboard").css('display', 'none');
+$("#driver_dashboard").css('display', 'none');
+$("#customer_dashboard").css('display', 'none');
+$("#reservation_dashboard").css('display', 'none');
+$("#payment_dashboard").css('display', 'none');
+$("#income_dashboard").css('display', 'none');
+$('#admin_profile').css('display', 'none');
+$("#schedule_dashboard").css('display', 'none');
+
+$("#btn_dashboard").click(function(){
+  $('#admin_dashboard').css('display', 'block');
+  $("#car_dashboard").css('display', 'none');
+  $("#driver_dashboard").css('display', 'none');
+  $("#customer_dashboard").css('display', 'none');
+  $("#reservation_dashboard").css('display', 'none');
+  $("#payment_dashboard").css('display', 'none');
+  $("#income_dashboard").css('display', 'none');
+  $('#admin_profile').css('display', 'none');
+
+
+});
