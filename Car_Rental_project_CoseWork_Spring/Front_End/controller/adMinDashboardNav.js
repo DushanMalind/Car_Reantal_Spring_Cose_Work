f2188f8efbff833ad83251adc16d74c067d4b26e
DushanMalind <dushanmalinda010@gmail.com>
2023-10-26 17:03:59 +0530

    btn_RESVISONDeatils js

--- Car_Rental_project_CoseWork_Spring/Front_End/controller/adMinDashboardNav.js
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/adMinDashboardNav.js
@@ -105,3 +105,19 @@ $("#btn_reservation").click(function(){
 
 });
 
+$("#btnExistReDetails").click(function () {
+  $('#admin_dashboard').css('display', 'none');
+  $("#car_dashboard").css('display', 'none');
+  $("#driver_dashboard").css('display', 'none');
+  $("#customer_dashboard").css('display', 'none');
+  $("#payment_dashboard").css('display', 'none');
+  $("#schedule_dashboard").css('display', 'none');
+  $("#income_dashboard").css('display', 'none');
+  $('#admin_profile').css('display', 'none');
+
+
+
+});
+
+
+
