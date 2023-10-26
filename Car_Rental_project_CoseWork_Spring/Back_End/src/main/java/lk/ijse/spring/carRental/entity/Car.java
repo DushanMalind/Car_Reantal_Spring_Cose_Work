6ddf4909dba0db4fe169c0b32f0589a80b4c3198
DushanMalind <dushanmalinda010@gmail.com>
2023-10-26 14:09:34 +0530

    adMinDashboard Update

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/entity/Car.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.carRental.entity;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 13:42
+ * 26/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.entity
+ */
+@AllArgsConstructor
+
+public class Car {
+
+}
