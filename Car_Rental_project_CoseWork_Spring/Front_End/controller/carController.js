a8e09a3d9e939df7d58816ad89176fee7eddea9f
DushanMalind <dushanmalinda010@gmail.com>
2023-10-27 15:41:29 +0530

    registrationNo event

---
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/carController.js
@@ -0,0 +1,26 @@
+let baseUrl="http://localhost:8081/Back_End_war/";
+
+
+var regDailyRatePrice = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+var regFKmDay = /^[0-9]{1,7}$/;
+var regExRegisterNO = /^(R00-)[0-9]{4}$/;
+var regPassengers = /^[1-9]{1,2}$/;
+var regExtraPrice = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+var regFKmMonth = /^[0-9]{1,7}$/;
+var regExDistance = /^[0-9]{1,5}$/;
+
+
+
+$("#registrationNo").on("keyup",function (event) {
+  let regNo=$("#registrationNo").val();
+  if(regExRegisterNO.test(regNo)) {
+    $("#registrationNo").css("border", "2px solid green");
+    $("#errorRegNo").text("");
+    if (event.key === "Enter") {
+      $("#noOfPassengers").focus();
+    }
+  }else {
+    $("#registrationNo").css("border", "2px solid red");
+    $("#errorRegNo").text("Invalid Registration No");
+  }
+});
