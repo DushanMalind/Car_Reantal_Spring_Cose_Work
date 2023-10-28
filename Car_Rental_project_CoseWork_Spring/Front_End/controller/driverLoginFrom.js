8322b43cd2820d1567f9d79f4af17383a91f27c3
DushanMalind <dushanmalinda010@gmail.com>
2023-10-28 20:29:15 +0530

    DriverloginFrom Js done

---
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/driverLoginFrom.js
@@ -0,0 +1,40 @@
+$("#btnLogToSystem").click(function (){
+  if ($("#loginUsername").val() == "" || $("#loginPassword").val() == "") {
+    alert("All Fields Are Required To Log !");
+  } else {
+    isExistsAdmin($("#loginUsername").val(), $("#loginPassword").val());
+  }
+});
+
+
+function isExistsAdmin(username, password) {
+  $.ajax({
+    url: "http://localhost:8081/Back_End_war/user/" + password + "/" + username,
+    method: "GET",
+    success: function (response) {
+      if (response.data.username == $("#loginUsername").val() && response.data.password == $("#loginPassword").val()) {
+        searchAdminTable(response.data.userId);
+      }
+      window.location.href = 'addMinDashBoard.html';
+    },
+    error: function (ob) {
+      alert("Wrong Username And Password !");
+    }
+  });
+}
+
+
+function searchAdminTable(usrId){
+  $.ajax({
+    url: "http://localhost:8081/Back_End_war/driver/USER/"+usrId,
+    method: "GET",
+    success: function (response) {
+      for (var i in response.data) {
+        if (response.data[i].username == $("#loginUsername").val() && response.data[i].password == $("#loginPassword").val()) {
+
+        }
+      }
+
+    }
+  });
+}
