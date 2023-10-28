28be5c173e3b6ba8a94b2a1ddc3f686d774c4f26
DushanMalind <dushanmalinda010@gmail.com>
2023-10-28 19:33:56 +0530

    DriverService interface ok

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/service/DriverService.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/service/DriverService.java
@@ -2,6 +2,8 @@ package lk.ijse.spring.carRental.service;
 
 import lk.ijse.spring.carRental.dto.DriverDTO;
 
+import java.util.List;
+
 /**
  * `@authority` DUSHAN MALINDA
  * 19:28
@@ -11,6 +13,24 @@ import lk.ijse.spring.carRental.dto.DriverDTO;
  * lk.ijse.spring.carRental.service
  */
 public interface DriverService {
+    void saveDriver(DriverDTO driver);
 
+    void updateDriver(DriverDTO driver);
+
+    void deleteDriver(String id);
+
+    DriverDTO searchDriver(String id);
+
+    List<DriverDTO> getAllDrivers();
+
+    String generateDriverIds();
+
+    DriverDTO searchUserFromDriver(String id);
+
+    DriverDTO assignRandomlyDriver(String release);
+
+    List<DriverDTO> searchAvailableDriversOrReleaseDrivers(String release);
+
+    int availableDriversOrOccupiedDrivers(String release);
 
 }
