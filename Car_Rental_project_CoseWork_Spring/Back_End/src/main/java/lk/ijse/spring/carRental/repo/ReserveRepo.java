429cdcb92d42e4f2882a34e0d4c7d19d6f06508f
DushanMalind <dushanmalinda010@gmail.com>
2023-10-30 19:10:02 +0530

    ReserveRepo

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/repo/ReserveRepo.java
@@ -0,0 +1,34 @@
+package lk.ijse.spring.carRental.repo;
+
+import lk.ijse.spring.carRental.entity.Reserve;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 18:59
+ * 30/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.repo
+ */
+public interface ReserveRepo extends JpaRepository<Reserve,String> {
+
+    @Query(value = "SELECT reserveId FROM Reserve ORDER BY reserveId DESC LIMIT 1",nativeQuery = true)
+    String generateReserveId();
+
+    @Query(value = "UPDATE ReserveDetails SET driverId=:driverId WHERE reserveId=:reserveId AND carId=:carId", nativeQuery = true)
+    void updateDriverId(@Param("driverId") String driverId, @Param("reserveId") String reserveId, @Param("carId") String carId);
+
+    @Query(value = "SELECT COUNT(*) FROM Reserve WHERE reserveDate=:date", nativeQuery = true)
+    int countDailyReservation(@Param("date") String date);
+
+    @Query(value = "SELECT COUNT(*) FROM Reserve WHERE requestAcceptOrDeny=:accept AND reserveDate=:date", nativeQuery = true)
+    int activeReservationPerDay(@Param("date") String date, @Param("accept") String accept);
+
+    @Query(value = "SELECT COUNT(*) FROM Reserve",nativeQuery = true)
+    int countAllReservation();
+
+
+}
