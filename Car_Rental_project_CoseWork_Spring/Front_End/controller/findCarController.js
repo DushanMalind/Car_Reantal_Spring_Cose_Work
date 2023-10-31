1b8390d7b7a29daf4303261feec1c5e47dccb64e
DushanMalind <dushanmalinda010@gmail.com>
2023-10-31 18:49:29 +0530

    customer booking details edit and update Chance

--- Car_Rental_project_CoseWork_Spring/Front_End/controller/findCarController.js
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/findCarController.js
@@ -153,5 +153,114 @@ function pasteDataToReservationFields() {
 }
 
 
+/*customer booking details edit and update Chance*/
 
+$("#pickUpDateEdit").keyup(function (event) {
+  let pickupDate = $("#pickUpDateEdit").val();
+  if (regExPickupDate.test(pickupDate)) {
+    $("#pickUpDateEdit").css('border', '2px solid #31d2f2');
+    $("#errorPickupDateAgain").text("");
+  } else {
+    $("#pickUpDateEdit").css('border', '2px solid red');
+    $("#errorPickupDateAgain").text("Check this field whether correct !");
+  }
+});
+
+
+$("#pickUpTimeEdit").keyup(function (event) {
+  let pickupTime = $("#pickUpTimeEdit").val();
+  if (regExPickupTime.test(pickupTime)) {
+    $("#pickUpTimeEdit").css('border', '2px solid #31d2f2');
+    $("#errorPickupTimeAgain").text("");
+  } else {
+    $("#pickUpTimeEdit").css('border', '2px solid red');
+    $("#errorPickupTimeAgain").text("Check this field whether correct !");
+  }
+});
+
+$("#returnDateEdit").keyup(function (event) {
+  let returnDate = $("#returnDateEdit").val();
+  if (regExReturnDate.test(returnDate)) {
+    $("#returnDateEdit").css('border', '2px solid #31d2f2');
+    $("#errorDropDateAgain").text("");
+  } else {
+    $("#returnDateEdit").css('border', '2px solid red');
+    $("#errorDropDateAgain").text("Check this field whether correct !");
+  }
+});
+
+
+$("#returnTimeEdit").keyup(function (event) {
+  let returnTime = $("#returnTimeEdit").val();
+  if (regExReturnTime.test(returnTime)) {
+    $("#returnTimeEdit").css('border', '2px solid #31d2f2');
+    $("#errorDroTimeAgain").text("");
+  } else {
+    $("#returnTimeEdit").css('border', '2px solid red');
+    $("#errorDroTimeAgain").text("Check this field whether correct !");
+  }
+});
+
+
+$("#pickUpLocationEdit").keyup(function (event) {
+  let pickupLocation = $("#pickUpLocationEdit").val();
+  if (regExPickupLocation.test(pickupLocation)) {
+    $("#pickUpLocationEdit").css('border', '2px solid #31d2f2');
+    $("#errorPickupLocationAgain").text("");
+  } else {
+    $("#pickUpLocationEdit").css('border', '2px solid red');
+    $("#errorPickupLocationAgain").text("Check this field whether correct !");
+  }
+});
+
+
+$("#returnLocationEdit").keyup(function (event) {
+  let returnLocation = $("#returnLocationEdit").val();
+  if (regExReturnLocation.test(returnLocation)) {
+    $("#returnLocationEdit").css('border', '2px solid #31d2f2');
+    $("#errorDropLocationAgain").text("");
+  } else {
+    $("#returnLocationEdit").css('border', '2px solid red');
+    $("#errorDropLocationAgain").text("Check this field whether correct !");
+  }
+});
+
+
+$("#destinationEdit").keyup(function (event) {
+  let destination = $("#destinationEdit").val();
+  if (regExDestination.test(destination)) {
+    $("#destinationEdit").css('border', '2px solid #31d2f2');
+    $("#errorDestinationAgain").text("");
+  } else {
+    $("#destinationEdit").css('border', '2px solid red');
+    $("#errorDestinationAgain").text("Check this field whether correct !");
+  }
+});
+
+
+$("#durationEdit").keyup(function (event) {
+  let duration = $("#durationEdit").val();
+  if (regExDuration.test(duration)) {
+    $("#durationEdit").css('border', '2px solid #31d2f2');
+    $("#errorDurationAgain").text("");
+  } else {
+    $("#durationEdit").css('border', '2px solid red');
+    $("#errorDurationAgain").text("Check this field whether correct !");
+  }
+});
+
+$("#editRentData").click(function () {
+  let text = "Do you want to Edit Data?";
+
+  if (confirm(text) == true) {
+    $("#pickUpDateEdit").prop("disabled", false);
+    $("#pickUpTimeEdit").prop("disabled", false);
+    $("#returnDateEdit").prop("disabled", false);
+    $("#returnTimeEdit").prop("disabled", false);
+    $("#pickUpLocationEdit").prop("disabled", false);
+    $("#returnLocationEdit").prop("disabled", false);
+    $("#destinationEdit").prop("disabled", false);
+    $("#durationEdit").prop("disabled", false);
+  }
+});
 
