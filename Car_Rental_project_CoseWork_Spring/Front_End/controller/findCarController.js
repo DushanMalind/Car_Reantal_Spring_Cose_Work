da6748b1acc8b7f4b6522c1b852b1b3e07fd3682
DushanMalind <dushanmalinda010@gmail.com>
2023-10-31 17:23:36 +0530

    findCarController regx resvison

--- Car_Rental_project_CoseWork_Spring/Front_End/controller/findCarController.js
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/findCarController.js
@@ -13,3 +13,29 @@ $("#PName").keyup(function (event) {
   }
 });
 
+
+$("#PContact").keyup(function (event) {
+  let contact = $("#PContact").val();
+  if (regExContact.test(contact)) {
+    $("#PContact").css('border', '2px solid #31d2f2');
+    if (event.key == "Enter") {
+      $("#PNIC").focus();
+    }
+  } else {
+    $("#PContact").css('border', '2px solid red');
+  }
+});
+
+
+$("#PNIC").keyup(function (event) {
+  let nic = $("#PNIC").val();
+  if (regExNIC.test(nic)) {
+    $("#PNIC").css('border', '2px solid #31d2f2');
+  } else {
+    $("#PNIC").css('border', '2px solid red');
+  }
+});
+
+
+
+$("#noResult").css('display', 'none');
