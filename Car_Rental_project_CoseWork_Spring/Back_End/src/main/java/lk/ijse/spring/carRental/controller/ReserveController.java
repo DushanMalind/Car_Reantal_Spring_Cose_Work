89540270b3e92642877f90a2d251c9d80e3e152d
DushanMalind <dushanmalinda010@gmail.com>
2023-10-31 12:21:56 +0530

    ReserveController class create

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/controller/ReserveController.java
@@ -0,0 +1,25 @@
+package lk.ijse.spring.carRental.controller;
+
+import lk.ijse.spring.carRental.service.ReserveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 12:19
+ * 31/10/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.controller
+ */
+@RestController
+@RequestMapping("/reserve")
+@CrossOrigin
+public class ReserveController {
+    @Autowired
+    ReserveService reserveService;
+
+
+}
