7faf2f24e3cda6d48df304bc93df40e2e73c309b
DushanMalind <dushanmalinda010@gmail.com>
2023-11-01 22:07:53 +0530

    admindashbord Details

---
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/addMinDashBordDetails.js
@@ -0,0 +1,8 @@
+let baseURLAdminPanel = "http://localhost:8081/Back_End_war/"
+
+
+var now = new Date();
+
+var day = ("0" + now.getDate()).slice(-2);
+var month = ("0" + (now.getMonth() + 1)).slice(-2);
+var today = now.getFullYear() + "-" + (month) + "-" + (day);
