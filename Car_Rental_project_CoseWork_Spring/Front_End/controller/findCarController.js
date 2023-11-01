b5104e2af89d2c8b7129c1e026370ae767a45215
DushanMalind <dushanmalinda010@gmail.com>
2023-11-01 16:52:42 +0530

    btnBook button add ok fil data

--- Car_Rental_project_CoseWork_Spring/Front_End/controller/findCarController.js
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/findCarController.js
@@ -2314,6 +2314,24 @@ var bookingDenyOrAccept;
 
 $("#btnBook").click(function () {
 
-
+  if ($("#PName").val() == "" || $("#PContact").val() == "" || $("#PNIC").val() == "") {
+    alert("Some personal details are not completed. So your booking is deny!..");
+    bookingDenyOrAccept = "Deny";
+  }else if ($('#uploadpaymentSlip').get(0).files.length === 0){
+    alert("Your Payment Slip are not Upload. So your booking is deny!..");
+    bookingDenyOrAccept = "Deny";
+  }else if (!regExFullName.test($("#PName").val()) || !regExContact.test($("#PContact").val()) || !regExNIC.test($("#PNIC").val())) {
+    alert("Some fields are not in correct format. So your booking is deny!..");
+    bookingDenyOrAccept = "Deny";
+  } else if ($("#BPickupDate").val() == "" || $("#BPickupTime").val() == "" || $("#BPickupLocation").val() == "" || $("#BReturnDate").val() == "" || $("#BReturnTime").val() == "" || $("#BReturnLocation").val() == "" || $("#BDestination").val() == "" || $("#BDuration").val() == "") {
+    alert("Some booking details fields are not completed. So your booking is deny!..");
+    bookingDenyOrAccept = "Deny";
+  } else if ($("#tblSelectedCars tbody tr").length == 0) {
+    alert("Table is empty. So your booking is deny!..");
+    bookingDenyOrAccept = "Deny";
+  } else {
+    bookingDenyOrAccept = "Accept";
+    findCustomerToReserve(bookingDenyOrAccept)
+  }
 
 });
