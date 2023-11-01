1267b67c7bf361a1225067719071c8703128a806
DushanMalind <dushanmalinda010@gmail.com>
2023-11-01 22:12:43 +0530

    totalNewUsers function

--- Car_Rental_project_CoseWork_Spring/Front_End/controller/addMinDashBordDetails.js
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/addMinDashBordDetails.js
@@ -6,3 +6,23 @@ var now = new Date();
 var day = ("0" + now.getDate()).slice(-2);
 var month = ("0" + (now.getMonth() + 1)).slice(-2);
 var today = now.getFullYear() + "-" + (month) + "-" + (day);
+
+
+function totalNewUsers() {
+  $.ajax({
+    url: baseURLAdminPanel + "customer/NewUsers/" + today,
+    method: "GET",
+    success: function (response) {
+      if (response.data == "") {
+        $("#totalNewUsers").text(0);
+      } else {
+        $("#totalNewUsers").text(response.data);
+      }
+    },
+    error: function (ob) {
+      alert(ob.responseJSON.message);
+    }
+  });
+}
+
+
