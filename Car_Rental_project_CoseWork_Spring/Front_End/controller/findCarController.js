07a9f3c0f51bad2d9160087c62293fafc723e674
DushanMalind <dushanmalinda010@gmail.com>
2023-11-01 17:35:20 +0530

    loadDriverSchedule ok resve

--- Car_Rental_project_CoseWork_Spring/Front_End/controller/findCarController.js
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/findCarController.js
@@ -2419,3 +2419,58 @@ function reserve(customer,bookingDenyOrAccept) {
   });
 
 }
+
+function loadDriverSchedule() {
+  console.log("schedule");
+  for (var i = 0; i < $("#tblSelectedCars tbody tr").length; i++) {
+
+    if ($('#checkDriverIfWant').is(':checked')) {
+      driverWantOrNot = "Want";
+    } else {
+      driverWantOrNot = "Not Want";
+    }
+
+    var reserveItems = {
+      reserveId: $("#reserveId").val(),
+      carId: $("#tblSelectedCars tbody tr").children(':nth-child(2)')[i].innerText,
+      driverId: $("#tblSelectedCars tbody tr").children(':nth-child(7)')[i].innerText,
+      type: $("#tblSelectedCars tbody tr").children(':nth-child(5)')[i].innerText,
+      colour: $("#tblSelectedCars tbody tr").children(':nth-child(4)')[i].innerText,
+      brand: $("#tblSelectedCars tbody tr").children(':nth-child(3)')[i].innerText,
+      driverWantOrNot:driverWantOrNot,
+      driverName: $("#tblSelectedCars tbody tr").children(':nth-child(8)')[i].innerText,
+      driverContact: $("#tblSelectedCars tbody tr").children(':nth-child(9)')[i].innerText,
+      loseDamageWaiverPayment: $("#tblSelectedCars tbody tr").children(':nth-child(10)')[i].innerText
+    }
+    console.log(reserveItems);
+
+    var schedule={
+      scheduleId:$("#scheduleId").val(),
+      pickUpDate:$("#BPickupDate").val(),
+      pickUpTime:$("#BPickupTime").val(),
+      returnDate:$("#BReturnDate").val(),
+      returnTime:$("#BReturnTime").val(),
+      pickUpVenue:$("#BPickupLocation").val(),
+      returnVenue:$("#BReturnLocation").val(),
+      releaseOrNot:"Not Release",
+      reserveDetails:reserveItems
+    }
+
+    console.log(schedule);
+
+    $.ajax({
+      url: baseURLForReservation+"schedule",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(schedule),
+      success: function (response) {
+        alert(response.message);
+        console.log("success");
+      },
+      error: function (ob) {
+        alert(ob.responseJSON.message);
+      }
+    });
+
+  }
+}
