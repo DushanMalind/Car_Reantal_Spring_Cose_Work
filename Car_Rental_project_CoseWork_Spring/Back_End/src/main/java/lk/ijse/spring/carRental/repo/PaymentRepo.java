26bc5357d060beed8495cf8af73fb3175ba2ad51
DushanMalind <dushanmalinda010@gmail.com>
2023-11-02 15:20:23 +0530

    PaymentRepo interface totalIncome method

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/repo/PaymentRepo.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/repo/PaymentRepo.java
@@ -16,4 +16,7 @@ public interface PaymentRepo extends JpaRepository<Payment,String> {
 
     @Query(value = "SELECT paymentId FROM Payment ORDER BY paymentId DESC LIMIT 1", nativeQuery = true)
     String generatePaymentId();
+
+    @Query(value = "SELECT SUM(fullPayment) FROM Payment", nativeQuery = true)
+    double totalIncome();
 }
