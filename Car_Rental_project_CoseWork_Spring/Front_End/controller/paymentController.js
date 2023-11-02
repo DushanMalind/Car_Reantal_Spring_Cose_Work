8156c6581eb95cd322b786698c1e0513ce6b631b
DushanMalind <dushanmalinda010@gmail.com>
2023-11-02 19:27:44 +0530

    paymentDate button create

--- Car_Rental_project_CoseWork_Spring/Front_End/controller/paymentController.js
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/paymentController.js
@@ -5,3 +5,18 @@ var regExPrice = /^[0-9]{1,10}(.)[0-9]{2}$/;
 var regExDate = /^\d{2}\/\d{2}\/\d{4}$/;
 var regExDistance = /^[0-9]{1,5}$/;
 
+
+$("#paymentDate").keyup(function (event) {
+  let date = $("#paymentDate").val();
+  if (regExDate.test(date)) {
+    $("#paymentDate").css('border', '2px solid #31d2f2');
+    $("#errorPaymentDate").text("");
+    if (event.key == "Enter") {
+      $("#rentFee").focus();
+    }
+  } else {
+    $("#paymentDate").css('border', '2px solid red');
+    $("#errorPaymentDate").text("Check this field whether correct !");
+  }
+});
+
