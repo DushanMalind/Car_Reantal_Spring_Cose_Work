315556f430c4413cdeafed7cc7a3ef6779c9d0ab
DushanMalind <dushanmalinda010@gmail.com>
2023-11-02 18:37:46 +0530

    PaymentController class @

--- Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/controller/PaymentController.java
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/controller/PaymentController.java
@@ -1,5 +1,11 @@
 package lk.ijse.spring.carRental.controller;
 
+import lk.ijse.spring.carRental.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * `@authority` DUSHAN MALINDA
  * 18:33
@@ -8,5 +14,13 @@ package lk.ijse.spring.carRental.controller;
  * Back_End
  * lk.ijse.spring.carRental.controller
  */
+
+@RestController
+@RequestMapping("/payment")
+@CrossOrigin
 public class PaymentController {
+    @Autowired
+    PaymentService paymentService;
+
+
 }
