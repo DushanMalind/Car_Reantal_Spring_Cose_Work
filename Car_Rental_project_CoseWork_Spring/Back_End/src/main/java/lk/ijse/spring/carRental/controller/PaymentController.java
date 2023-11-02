42f44e17fcb358c1b9f3e5432c1fa373f97ae989
DushanMalind <dushanmalinda010@gmail.com>
2023-11-02 18:33:36 +0530

    PaymentController class

---
+++ Car_Rental_project_CoseWork_Spring/Back_End/src/main/java/lk/ijse/spring/carRental/controller/PaymentController.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.carRental.controller;
+
+/**
+ * `@authority` DUSHAN MALINDA
+ * 18:33
+ * 02/11/2023
+ * ASUS
+ * Back_End
+ * lk.ijse.spring.carRental.controller
+ */
+public class PaymentController {
+}
