967c4cfa1077ff2a24babde841a8519f42644a66
DushanMalind <dushanmalinda010@gmail.com>
2023-11-02 19:15:24 +0530

    PaymentController Js part

---
+++ Car_Rental_project_CoseWork_Spring/Front_End/controller/paymentController.js
@@ -0,0 +1,7 @@
+let baseURLForPayment="http://localhost:8081/Back_End_war/"
+
+
+var regExPrice = /^[0-9]{1,10}(.)[0-9]{2}$/;
+var regExDate = /^\d{2}\/\d{2}\/\d{4}$/;
+var regExDistance = /^[0-9]{1,5}$/;
+
